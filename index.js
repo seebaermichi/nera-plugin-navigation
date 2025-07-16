4f68e6dd9e09e496f0bf1675be42069495dd5257
Michael Becker <info@michael-becker-berlin.de>
2025-07-16 09:40:39 +0200

    Fix code style

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-import path from 'path';
-import { getConfig } from '@nera-static/plugin-utils';
+import path from 'path'
+import { getConfig } from '@nera-static/plugin-utils'
 
 // Default location to look for navigation.yaml in host project
-const HOST_CONFIG_PATH = path.resolve(process.cwd(), 'config/navigation.yaml');
+const HOST_CONFIG_PATH = path.resolve(process.cwd(), 'config/navigation.yaml')
 
 function getNavElements(elements) {
     return elements.map((element) => ({
         ...element,
         path: path.posix.dirname(element.href),
-    }));
+    }))
 }
 
 function getMainNav() {
-    const navConfig = getConfig(HOST_CONFIG_PATH);
+    const navConfig = getConfig(HOST_CONFIG_PATH)
 
-    navConfig.activeClass = navConfig.active_class || 'active';
-    navConfig.activePathClass = navConfig.active_path_class || 'active-path';
-    navConfig.navClass = navConfig.nav_class || 'nav';
+    navConfig.activeClass = navConfig.active_class || 'active'
+    navConfig.activePathClass = navConfig.active_path_class || 'active-path'
+    navConfig.navClass = navConfig.nav_class || 'nav'
 
     if (Array.isArray(navConfig.elements)) {
-        navConfig.elements = getNavElements(navConfig.elements);
+        navConfig.elements = getNavElements(navConfig.elements)
     } else {
         for (const key in navConfig.elements) {
             navConfig[key] = {
                 className: `${key}-${navConfig.navClass}`,
                 elements: getNavElements(navConfig.elements[key]),
-            };
+            }
         }
     }
 
-    return navConfig;
+    return navConfig
 }
 
 export function getAppData(data) {
@@ -37,8 +37,8 @@ export function getAppData(data) {
         return {
             ...data.app,
             nav: getMainNav(),
-        };
+        }
     }
 
-    return data.app;
+    return data.app
 }
